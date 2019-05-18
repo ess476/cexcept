23f41eb734c3f112c16a6198ed8364d03a21c7f7
Ethan Sattler <ethan_sattler@brown.edu>
2019-05-18 01:16:14 -0400

    added deep nesting

--- cexcept.h
+++ cexcept.h
@@ -5,9 +5,17 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#define CEXCEPT_MAX_DEPTH 1024
+typedef struct __exception_ctx {
+    jmp_buf jmp_ctx;
+    struct __exception_ctx* prev;
+    struct __exception_ctx* next;
+    int code;
+    int active;
+    void* obj;
+} __exception_ctx_t;
 
-jmp_buf __exception_ctx_stack[CEXCEPT_MAX_DEPTH];
+__exception_ctx_t* __exception_cur_ctx;
+__exception_ctx_t* __exception_tmp_ctx;
 
 volatile int __exception_depth = 0;
 
@@ -20,14 +28,6 @@ void __exception_handler_dfl(int code) {
     __exception_exit(code);
 }
 
-int static inline __exception_depth_check() {
-    if (__exception_depth == CEXCEPT_MAX_DEPTH) {
-        __exception_exit(1);
-    }
-
-    return 1;
-}
-
 #define CEXCEPT_DFL = __exception_handler_dfl;
 
 void (*__exception_handler)(int) = __exception_handler_dfl;
@@ -36,15 +36,80 @@ void cxecpt_handler(void (*handler)(int)) {
     __exception_handler = handler;
 }
 
-#define try if (__exception_depth_check() && (__exception_ret = setjmp(__exception_ctx_stack[__exception_depth++])) == 0)
 
-#define throw(code) do { if (__exception_depth) {longjmp(__exception_ctx_stack[--__exception_depth], code); } else { __exception_handler(code); } } while(0)
+#define __exception_pop() \
+    __exception_depth--; \
+    __exception_cur_ctx->active = 0; \
+    __exception_tmp_ctx = __exception_cur_ctx; \
+    __exception_cur_ctx = __exception_cur_ctx->prev;
 
-#define catch0() else if ((errno = __exception_ret))
 
-#define catch1(var) else if ((var = __exception_ret))
+void __exception_out_of_scope(__exception_ctx_t* ctx) {
+    if (ctx->active) {
+        __exception_pop();
+    }
+}
 
-#define GET_MACRO(_0, _1, NAME, ...) NAME
-#define catch(...) GET_MACRO(_0, ##__VA_ARGS__, catch1, catch0)(__VA_ARGS__)
+#define try { \
+    __exception_tmp_ctx = __exception_cur_ctx; \
+    __exception_ctx_t __attribute__((cleanup (__exception_out_of_scope))) tmp; \
+    __exception_cur_ctx = &tmp; \
+    __exception_cur_ctx->code = 0; \
+    __exception_cur_ctx->active = 1; \
+    __exception_cur_ctx->obj = NULL; \
+    __exception_cur_ctx->prev = __exception_tmp_ctx; \
+    __exception_cur_ctx->next = NULL; \
+    if (__exception_tmp_ctx) { \
+        __exception_tmp_ctx->next = __exception_cur_ctx; \
+    } \
+    __exception_depth++; \
+    if ((__exception_ret = setjmp(__exception_cur_ctx->jmp_ctx)) == 0)
+
+#define throw1(__code) \
+    do { \
+        if (__exception_depth) { \
+            __exception_pop(); \
+            __exception_tmp_ctx->code = __code; \
+            __exception_tmp_ctx->obj = NULL; \
+            longjmp(__exception_tmp_ctx->jmp_ctx, 1); \
+        } else { \
+            __exception_handler(__code); \
+        } \
+    } while(0)
+
+#define throw2(__code, __obj) \
+    do { \
+        if (__exception_depth) { \
+            __exception_pop(); \
+            __exception_tmp_ctx->code = __code; \
+            __exception_tmp_ctx->obj = (void*) __obj; \
+            longjmp(__exception_tmp_ctx->jmp_ctx, 1); \
+        } else { \
+            __exception_handler(__code); \
+        } \
+    } while(0)
+
+#define GET_THROW_MACRO(_1, _2, NAME, ...) NAME
+#define throw(...) GET_THROW_MACRO(__VA_ARGS__, throw2, throw1)(__VA_ARGS__)
+
+#define catch0() \
+    } if (!__exception_ret) { \
+        __exception_pop(); \
+    } else if (((errno = __exception_tmp_ctx->code) || 1) && !(__exception_ret = 0))
+
+#define catch1(__code) \
+    } if (!__exception_ret) { \
+        __exception_pop(); \
+    } else if (((__code = __exception_tmp_ctx->code) || 1) && !(__exception_ret = 0))
+
+#define catch2(__code, __obj) \
+    } __obj = NULL; \
+    if (!__exception_ret) { \
+        __exception_pop(); \
+    } else if ((((__code = __exception_tmp_ctx->code) && (__obj = __exception_tmp_ctx->obj)) || 1) && !(__exception_ret = 0))
+
+
+#define GET_CATCH_MACRO(_0, _1, _2, NAME, ...) NAME
+#define catch(...) GET_CATCH_MACRO(_0, ##__VA_ARGS__, catch2, catch1, catch0)(__VA_ARGS__)
 
 #endif //CEXCEPT_CEXCEPT_H
--- main.c
+++ main.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
 #include "cexcept.h"
 
-void bomb(int x) {
+int fact(int x) {
 
+    printf("fact(%d)\n", x);
     if (x < 0) {
-        throw(456);
+        throw(x);
     }
 
-    bomb(x - 1);
+    try {
+        return x * fact(x - 1);
+    } catch(errno) {
+        printf("catch: %x\n", x);
+        return 1;
+    }
+}
+
+int test() {
+    char* x = "Hello, World!";
+
+    try {
+
+        char* msg;
+        try {
+
+            printf("fact(5): %d\n", fact(5));
+
+        } catch(errno, msg) {
+
+            printf("catch 1: %d : %s\n", errno, msg);
+
+            return 183;
+        }
+
+        throw(111);
+    } catch(errno) {
+        printf("catch 2: %d\n", errno);
+
+        return 123;
+
+    }
+
+    return 4;
 }
 
 int main() {
 
-    try {
-        try {
-
-            bomb(15);
-
-        } catch(errno) {
-
-            printf("catch 1: %d\n", errno);
-            throw(789);
-
-        }
-    } catch(errno) {
-        printf("catch 2: %d\n", errno);
-    }
+    int x = test();
+    printf("%d\n", x);
 }
